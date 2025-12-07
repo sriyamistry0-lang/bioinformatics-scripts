cfc9461cf87df4b295d0560a7c3e5fea05eb7233
sriyamistry0-lang <sriyamistry0@gmail.com>
2025-12-07 19:01:52 +0530

    Calculate_gc_content_calculator.py

---
+++ gc_content_calculator.py
@@ -0,0 +1,12 @@
+#gc_content_calculator.py
+#Code_to_cal_gc_content_manually
+#define_seq
+dna_seq="ATGCATGCATGCATCGATCGATCGGCGCGCGCGCTA"
+#count_g's_c'c
+g_count = dna_seq.count('G')
+c_count = dna_seq.count('C')
+total_length = len(dna_seq)
+gc_per = (g_count +c_count)/total_length * 100
+print(f"DNA seq: {dna_seq})
+print(f"Total length: {total_length})
+print(f"GC_Content: {gc_per:.2f}%)
